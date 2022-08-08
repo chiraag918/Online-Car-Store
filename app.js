9b4ae1cdf83216dfc3e7179d174caab008983671
Chiraag_Arun_Kumar <39455997+chiraag918@users.noreply.github.com>
2022-08-08 14:09:45 +0530

    Add comments to HTML/CSS/JS files

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-let imageSrc={
+
+//Constants used to render the cart items
+const IMAGE_SRC={
   Lamborghini_Aventador_SVJ:"images/lamb.jpeg",
   Audi_R8:"images/R8.jpeg",
   BMW_M3:"images/M3.jpeg",
@@ -7,16 +9,8 @@ let imageSrc={
   Mercedes_Benz_G63_AMG:"images/G63.jpeg"
 };
 
-let prices={
-  Lamborghini_Aventador_SVJ:180000,
-  Audi_R8:150000,
-  BMW_M3:110000,
-  Toyota_LC300_GR_Sport:100000,
-  Mercedes_Benz_S600:120000,
-  Mercedes_Benz_G63_AMG:115000
-};
 
-let brandDisc={
+const BRAND_DISC={
   Lamborghini_Aventador_SVJ:1,
   Audi_R8:2,
   BMW_M3:2.5,
@@ -25,7 +19,7 @@ let brandDisc={
   Mercedes_Benz_G63_AMG:1
 };
 
-let compDisc={
+let COMP_DISC={
   Lamborghini_Aventador_SVJ:0.1,
   Audi_R8:0.15,
   BMW_M3:0.3,
@@ -35,7 +29,7 @@ let compDisc={
 };
 
 
-
+//Query selectors for ham-burger animation & menu dropdown - when screen size small
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 
@@ -52,6 +46,7 @@ cart_button.addEventListener('click', function() {
   cart_menu.classList.toggle('active')
 })
 
+//DOM manipulation to shift between home & gallery sections - display property - changed
 document.getElementById("explore").addEventListener("click", displayBlockMain);
 document.getElementById("explore_2").addEventListener("click", displayBlockMain);
 
@@ -72,24 +67,28 @@ function displayBlockExplore() {
 }
 
 
-  var removeCartItemButtons = document.getElementsByClassName('btn-danger')
+  //event listener on remove button on cart items
+  var removeCartItemButtons = document.getElementsByClassName('btn-remove')
   for (var i = 0; i < removeCartItemButtons.length; i++) {
       var button = removeCartItemButtons[i]
       button.addEventListener('click', removeCartItem)
   }
-
+ 
+  //event listener on cart quantity input box on cart items
   var quantityInputs = document.getElementsByClassName('cart-quantity-input')
   for (var i = 0; i < quantityInputs.length; i++) {
       var input = quantityInputs[i]
       input.addEventListener('change', quantityChanged)
   }
 
+  //event listener on cart quantity input box on gallery page for each item
   var quantityInputsPage = document.getElementsByClassName('cart-quantity-input-page')
   for (var i = 0; i < quantityInputsPage.length; i++) {
       var input = quantityInputsPage[i]
       input.addEventListener('change', quantityChangedPage)
   }
 
+  //event listener on add to cart button on gallery page for each item
   var addToCartButtons = document.getElementsByClassName('shop-item-button')
   console.log(addToCartButtons)
   for (var i = 0; i < addToCartButtons.length; i++) {
@@ -98,6 +97,7 @@ function displayBlockExplore() {
       button.addEventListener('click', addToCartClicked)
   }
 
+  //event listener on increment button for qty on gallery page for each item
   var incrementCartButton = document.getElementsByClassName('button-range-increment')
   for (var i = 0; i < incrementCartButton.length; i++) {
       var button = incrementCartButton[i]
@@ -120,43 +120,43 @@ function displayBlockExplore() {
 
   }
 
-  var decrementCartButton = document.getElementsByClassName('button-range-decrement')
-  for (var i = 0; i < decrementCartButton.length; i++) {
-      var button = decrementCartButton[i]
-      
-      button.addEventListener('click', decrementButtonClicked)
-  }
+//event listener on decrement button for qty on gallery page for each item
+var decrementCartButton = document.getElementsByClassName('button-range-decrement')
+for (var i = 0; i < decrementCartButton.length; i++) {
+  var button = decrementCartButton[i]
 
-  function decrementButtonClicked(event){
-    var buttonClicked = event.target;
-    console.log(buttonClicked);
-    
-    var cartCountElement = buttonClicked.parentElement.getElementsByClassName("cart-quantity-input-page")[0];
-    
-    if(cartCountElement.value>1){
-     cartCountElement.value--;
+  button.addEventListener('click', decrementButtonClicked)
+}
+
+function decrementButtonClicked(event) {
+  var buttonClicked = event.target;
+  console.log(buttonClicked);
+
+  var cartCountElement = buttonClicked.parentElement.getElementsByClassName("cart-quantity-input-page")[0];
+
+  if (cartCountElement.value > 1) {
+    cartCountElement.value--;
 
     var carTitle = buttonClicked.parentElement.parentElement.getElementsByClassName("shop-item-title")[0].innerText;
-    
+
     updateCartQty(carTitle, cartCountElement.value);
 
     updateCartTotal();
-    }
-    else
-     {
-      var directParent = buttonClicked.parentElement;
-
-      directParent.style.display="none";
-      directParent.parentElement.getElementsByClassName("shop-item-button")[0].style.display="block";
-
-      var carTitle = directParent.parentElement.getElementsByClassName("shop-item-title")[0].innerText;
-
-      removeCartItemByName(carTitle);
-     }
   }
+  else {
+    var directParent = buttonClicked.parentElement;
 
+    directParent.style.display = "none";
+    directParent.parentElement.getElementsByClassName("shop-item-button")[0].style.display = "block";
 
-  document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
+    var carTitle = directParent.parentElement.getElementsByClassName("shop-item-title")[0].innerText;
+
+    removeCartItemByName(carTitle);
+  }
+}
+
+//event listener on purchase button on the cart menu 
+document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
 
 
 function purchaseClicked() {
@@ -168,6 +168,9 @@ function purchaseClicked() {
   updateCartTotal()
 }
 
+//Helper Functions
+
+//To remove cart item row on cart menu
 function removeCartItemByName(carTitle){
 
       var cartItemElement = searchCartItem(carTitle);
@@ -175,6 +178,7 @@ function removeCartItemByName(carTitle){
       updateCartTotal();
 }
  
+//To search for cart element on cart menu based on title 
 function searchCartItem(carTitle){
   var allCartItems = document.getElementsByClassName("cart-row");
 
@@ -187,7 +191,7 @@ function searchCartItem(carTitle){
 }
 
 
-
+//To remove cart item row on cart menu from the gallery page - when qty range is decreased further 1
 function removeCartItem(event) {
   var buttonClicked = event.target
   var carTitle = buttonClicked.parentElement.parentElement.getElementsByClassName("cart-item-title")[0].innerText;
@@ -197,6 +201,7 @@ function removeCartItem(event) {
   updateCartTotal()
 }
 
+//Toggles between add cart button and qty range on gallery page
 function removeCartRange(carTitle)
 {
   itemElement = searchCarItem(carTitle);
@@ -207,6 +212,7 @@ function removeCartRange(carTitle)
 
 }
 
+//To search for cart element on gallery page
 function searchCarItem(carTitle)
 {
   allCarItems = document.getElementsByClassName("car_info");
@@ -221,6 +227,7 @@ function searchCarItem(carTitle)
  
 }
 
+//Callback on qty input box on cart item event handler 
 function quantityChanged(event) {
   var input = event.target
   if (isNaN(input.value) || input.value <= 0) {
@@ -232,12 +239,14 @@ function quantityChanged(event) {
   updatePageQty(carTitle, input.value);
 }
 
+//To sync value with qty input box on gallery page when quantityChanged is invoked
 function updatePageQty(carTitle, value)
 {
   var itemElement = searchCarItem(carTitle);
   itemElement.getElementsByClassName("cart-quantity-input-page")[0].value=value;
 }
 
+//Callback on qty input box on gallery page event handler
 function quantityChangedPage(event) {
   var input = event.target
   if (isNaN(input.value) || input.value <= 0) {
@@ -250,12 +259,14 @@ function quantityChangedPage(event) {
   updateCartTotal()
 }
 
+//To sync value with qty input box on cart item when quantityChangedPage is invoked
 function updateCartQty(carTitle, value)
 {
   var itemElement = searchCartItem(carTitle);
   itemElement.parentElement.parentElement.getElementsByClassName("cart-quantity-input")[0].value=value;
 }
 
+//Callback to add to cart button event handler
 function addToCartClicked(event) {
   var button = event.target
   button.style.display="none";
@@ -270,6 +281,7 @@ function addToCartClicked(event) {
   updateCartTotal()
 }
 
+//Function to add cart element onto the cart menu
 function addItemToCart(title, price) 
   {var cartRow = document.createElement('div')
   cartRow.classList.add('cart-row')
@@ -280,7 +292,7 @@ function addItemToCart(title, price)
   let keyImage=arr.join('_');
 
   console.log(keyImage);
-  console.log(imageSrc[keyImage]);
+  console.log(IMAGE_SRC[keyImage]);
   
   for (var i = 0; i < cartItemNames.length; i++) {
       if (cartItemNames[i].innerText == title) {
@@ -290,22 +302,23 @@ function addItemToCart(title, price)
   }
   var cartRowContents = `
       <div class="cart-item cart-column">
-          <img class="cart-item-image" src=${imageSrc[keyImage]} width="100" height="100">
+          <img class="cart-item-image" src=${IMAGE_SRC[keyImage]} width="100" height="100">
           <span class="cart-item-title">${title}</span>
       </div>
       <span class="cart-price cart-column">${price}</span>
-      <span class="cart-brand-disc cart-column">${brandDisc[keyImage]}%</span>
-      <span class="cart-company-disc cart-column">${compDisc[keyImage]}%</span>
+      <span class="cart-brand-disc cart-column">${BRAND_DISC[keyImage]}%</span>
+      <span class="cart-company-disc cart-column">${COMP_DISC[keyImage]}%</span>
       <div class="cart-quantity cart-column">
           <input class="cart-quantity-input" type="number" value="1">
-          <button class="btn btn-danger" type="button">X</button>
+          <button class="btn btn-remove" type="button">X</button>
       </div>`
   cartRow.innerHTML = cartRowContents
   cartItems.append(cartRow)
-  cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
+  cartRow.getElementsByClassName('btn-remove')[0].addEventListener('click', removeCartItem)
   cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
 }
 
+//Function to update the total value on cart menu
 function updateCartTotal() {
   var cartItemContainer = document.getElementsByClassName('cart-items')[0]
   var cartRows = cartItemContainer.getElementsByClassName('cart-row')
